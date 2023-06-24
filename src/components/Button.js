32f4e9fee29cc2fc2ad0ca34070eee1cb5004e20
Aline Fernanda Bellozo <alinebellozo@gmail.com>
2023-06-23 19:33:34 -0700

    Build the DayListItem:
    - create the DayListItem component,
    - write the stories;
    - implement the props;
    - update ESlint version due to errors.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,21 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
+  const buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger,
+  });
 
-   let buttonClass = classNames("button", {
-     "button--confirm": props.confirm,
-     "button--danger": props.danger,
-   });
-
-   return (
-     <button
-       onClick={props.onClick}
-       className={buttonClass}
-       disabled={props.disabled}
-     >
-       {props.children}
-     </button>
-   );
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
