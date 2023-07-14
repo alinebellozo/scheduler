9a53388f7dade9e06c82e27ca1ed1dcf98615014
Aline Fernanda Bellozo <alinebellozo@gmail.com>
2023-07-14 16:48:12 -0700

    Build a Hook:
    - create the hooks directory, and a useVisualMode.js file inside it;
    - create the __tests__ directory inside hookes, and useVisualMode.test.js file inside it;
    - install react-hooks-testing-library and react-test-renderer;
    - implement the useVisualMode function;
    - implement the transition and back functions within the useVisualMode function;
    - add tests to check the code implemented.

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  
+  // history array will always need to have a length that is greater than or equal to 1.
+
+  function transition(newMode, replace = false) {
+
+    if(!replace) {
+      setMode(newMode);
+      history.push(newMode);
+    } else {
+      setMode(newMode);
+    }
+
+    return {};
+  }
+
+  function back() {
+    if (history.length > 1) {
+      setMode(history[history.length - 2]);
+      setHistory(history.slice(0, -1));
+    } else {
+      setHistory(history);
+    }
+  }
+
+  return { mode, transition, back };
+}
