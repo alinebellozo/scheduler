fae83722d14b3e3fed587acbf7ef43039d3ccd5c
Aline Fernanda Bellozo <alinebellozo@gmail.com>
2023-07-21 19:49:31 -0700

    Debug tests:
    - add more tests to the Application.test.js file;
    - update the DayListItem.js file.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -12,19 +12,21 @@ export default function DayListItem(props) {
 
   const formatSpots = (numOfSpots) => {
     if (numOfSpots === 0) {
-      console.log("numOfSpots 1", numOfSpots);
       return "no spots remaining";
     }
     if (numOfSpots === 1) {
-      console.log("numOfSpots 2", numOfSpots);
       return "1 spot remaining";
     }
-    console.log("numOfSpots 3", numOfSpots);
+    
     return `${numOfSpots} spots remaining`;
   };
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li
+      className={dayClass}
+      data-testid="day"
+      onClick={() => props.setDay(props.name)}
+    >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
